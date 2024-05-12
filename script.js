a08967f987e9ab1717fe34bcf43075b54195dc96
Ankan Maity <ankanmaity27@gmail.com>
2024-05-12 19:36:56 +0530

    Error handling added

--- script.js
+++ script.js
@@ -6,36 +6,42 @@ const weatherImg=document.querySelector(".weather-icon");
 
 async function getweather(city){
     const response=await fetch(urlapi+city+`&appid=${apikey}`);
-    var data= await response.json();
 
-    console.log(data);
+    if(response.status==404){
+        document.querySelector(".weather").style.display="none";
+        document.querySelector(".error").style.display="block";
+    }
+    else{
+        var data= await response.json();
 
-    document.querySelector(".city").innerHTML=data.name;
-    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°c";
-    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
-    document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+        //console.log(data);
 
-    if(data.weather[0].main=="Clouds"){
-        weatherImg.src="images/clouds.png";
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°c";
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+
+        if(data.weather[0].main=="Clouds"){
+            weatherImg.src="images/clouds.png";
+        }
+        else if(data.weather[0].main=="Rain"){
+            weatherImg.src="images/rain.png";
+        }
+        else if(data.weather[0].main=="Snow"){
+            weatherImg.src="images/snow.png";
+        }
+        else if(data.weather[0].main=="Mist"){
+            weatherImg.src="images/mist.png";
+        }
+        else if(data.weather[0].main=="Clear"){
+            weatherImg.src="images/clear.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            weatherImg.src="images/drizzle.png";
+        }
+        document.querySelector(".error").style.display="none";
+        document.querySelector(".weather").style.display="block";
     }
-    else if(data.weather[0].main=="Rain"){
-        weatherImg.src="images/rain.png";
-    }
-    else if(data.weather[0].main=="Snow"){
-        weatherImg.src="images/snow.png";
-    }
-    else if(data.weather[0].main=="Mist"){
-        weatherImg.src="images/mist.png";
-    }
-    else if(data.weather[0].main=="Clear"){
-        weatherImg.src="images/clear.png";
-    }
-    else if(data.weather[0].main=="Drizzle"){
-        weatherImg.src="images/drizzle.png";
-    }
-    document.querySelector(".weather").style.display="block";;
-    
-    
 }
 
 searchbutton.addEventListener("click",()=>{
